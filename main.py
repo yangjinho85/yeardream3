cb809c76bc0f0b29b7c19690e2925c69973bdde5
yjh <fenel@naver.com>
2023-05-01 11:21:55 +0900

    feat : Define print_hello(),(Trailing comma test) ``` print hello to all names in names. ```

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-#이제 이걸 파이썬 메인으로 정했다 
+# Test python env
+def print_hello():
+    animals = ['dog','cat','hamster'] # in one line
+    foods = [
+        'Spagetti',
+        'Pizza'
+    ] # w/o trailing comma
+    names = [
+        'John',
+        'Jane',
+        'Gil-dong',
+    ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()#이제 이걸 파이썬 메인으로 정했다 
 
 print("hello")
